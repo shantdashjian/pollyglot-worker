a6007ebc2d8e27f30172eb100dd703325925e486
Shant Dashjian <shantgdashjian@gmail.com>
2025-04-04 13:54:41 +0400

    Limit requests from the frontend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import OpenAI from 'openai'
 
 const corsHeaders = {
-	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Origin': 'https://pollyglot-app.pages.dev',
 	'Access-Control-Allow-Methods': 'POST, OPTIONS',
 	'Access-Control-Allow-Headers': 'Content-Type',
 }
